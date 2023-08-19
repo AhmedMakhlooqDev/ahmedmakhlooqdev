a6bb0104e6fe52bc38a0cecb87f8f64e471c0836
AhmedMakhlooqDev <ahmedmakhlooqdev@gmail.com>
2023-08-19 17:07:48 +0300

    did some black magic

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // script.js
 
 
-const toggleButton = document.querySelector('.toggle-button');
+// const toggleButton = document.querySelector('.toggle-button');
 const body = document.querySelector('body');
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-});
+// toggleButton.addEventListener('click', () => {
+//   body.classList.toggle('dark-mode');
+// });
 
 
 // Listener to move within pages 
@@ -23,4 +23,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+
+document.addEventListener('mousemove', function(event) {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  
+  var mouseXpercentage = Math.round((event.pageX / windowWidth) * 100);
+  var mouseYpercentage = Math.round((event.pageY / windowHeight) * 100);
+
+  // Adjust the radius to control the gradient size (smaller radius -> smaller gradient)
+  var gradientRadius = '350px';
+
+  var radialGradient = document.querySelector('.radial-gradient');
+  radialGradient.style.background = 'radial-gradient(circle ' + gradientRadius + ' at '
+    + mouseXpercentage + '% ' + mouseYpercentage + '%, #293b64, #0F172A)';
+});
+
 
